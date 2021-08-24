7fc8d1d694992194ca59a7ec777f8a7031f637c4
sdu-gyf <48801944+sdu-gyf@users.noreply.github.com>
2021-08-24 23:02:19 +0800

    fix json loads error

--- health_report_helper/main.py
+++ health_report_helper/main.py
@@ -12,7 +12,10 @@ from uniform_login.uniform_login_spider import login
 import time
 
 if __name__ == '__main__':
-    config.data = json.loads(re.sub('#(.*)\n', '\n', sys.argv[1]).replace("'", '"'))
+    try:
+        config.data = json.loads(re.sub('#(.*)\n', '\n', sys.argv[1]).replace("'", '"'))
+    except:
+        config.data = eval(sys.argv[1])
     JSESSIONID = "0"
     while len(JSESSIONID) < 63:
         JSESSIONID=login(config.data['username'], config.data['password'], 'https://scenter.sdu.edu.cn/tp_fp/view?m=fp')
